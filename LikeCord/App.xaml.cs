54587490a9948a48e727c13bffb5abd5d8d9be09
Vlad Ognev <firehead16@gmail.com>
2024-10-13 00:38:55 +0500

    added MAUI

---
+++ LikeCord/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LikeCord
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ LikeCord/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LikeCord
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
